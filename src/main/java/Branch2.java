f9a541c27e7b0ab9aa0b0e29afc99b60c6377bce
shireesha66 <shirishareddy66@gmail.com>
2025-03-07 10:12:05 -0500

    added new lines of code

---
+++ src/main/java/Branch2.java
@@ -0,0 +1,9 @@
+
+public class Branch2 {
+	public static void main(String[] args) {
+		System.out.println("branch2");
+		System.out.println("hello2");
+	}
+
+
+}
---
+++ src/main/java/branch3.java
@@ -0,0 +1,7 @@
+
+public class branch3 {
+	public static void main(String[] args) {
+		System.out.println("branch3");
+	}
+
+}
