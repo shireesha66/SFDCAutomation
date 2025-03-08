30539bae04e740adb5ecb310292a4833e9f2668d
shireesha66 <shirishareddy66@gmail.com>
2025-03-08 11:14:35 -0500

    added substraction method

--- src/main/java/Branch2.java
+++ src/main/java/Branch2.java
@@ -3,6 +3,13 @@ public class Branch2 {
 	public static void main(String[] args) {
 		System.out.println("branch2");
 		System.out.println("hello2");
+		
+	}
+	public void sub() {
+		int a=3;
+		int b=1;
+		int c= a-b;
+		System.out.println("sub"+ c);
 	}
 
 
