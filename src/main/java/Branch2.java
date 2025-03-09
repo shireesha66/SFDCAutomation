390fac27e155ced8fe73b81e11e957e06211d44c
shireesha66 <shirishareddy66@gmail.com>
2025-03-09 06:44:37 -0400

    new dots added

--- src/main/java/Branch2.java
+++ src/main/java/Branch2.java
@@ -2,6 +2,8 @@
 public class Branch2 {
 	public static void main(String[] args) {
 		System.out.println("branch2");
+
+
 		System.out.println("hello2");
 		
 	}
@@ -12,6 +14,9 @@ public class Branch2 {
 		int d=a*b;
 		System.out.println("sub"+ c);
 		System.out.println(d);
+
+		System.out.println("hello2......");
+
 	}
 
 
